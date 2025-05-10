578fbb794a9ce57ab21298197b82324ec34dac1b
RafaelCuya <u201913495@upc.edu.pe>
2025-05-10 12:54:58 -0500

    Feat/shared) routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from '../public/navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: NavbarComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
