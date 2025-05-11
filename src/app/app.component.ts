6be3eb8a1c2256871656e731a9599ee36e0d8150
Moris0306 <francomauricio2004@gmail.com>
2025-05-11 18:51:56 -0500

    feat: Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';	
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    MatCardModule,
+    MatTabsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-frontend';
+  title = 'Login';
+  email: string = '';
+  password: string = '';
+  registerEmail: string = '';
+  registerPassword: string = '';
+  registerconfirmPassword: string = '';
+  register(){
+
+  }
+  login(){
+    if(this.email == 'admin' && this.password == 'admin'){
+      alert('Login Success');
+    }
+  }
+
 }
