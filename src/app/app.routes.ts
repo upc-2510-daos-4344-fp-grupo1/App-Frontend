3bf2682591d0daed57c0872423f95533df698ebf
DamianIH <u201924756@upc.edu.pe>
2025-05-11 23:22:43 -0500

    feat(authentication) Created profile components
    Se creo un componente para el perfil de usuario y un componente para el perfil de empresa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { CompanyProfileComponent } from './Authentication/CompanyProfile/company-profile/company-profile.component';
+import { ProfileComponent } from './Authentication/Profile/profile/profile.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'company',
+        component: CompanyProfileComponent
+    },
+    {
+        path: 'profile',
+        component: ProfileComponent
+    }
+];
