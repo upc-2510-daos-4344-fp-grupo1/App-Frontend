3b0871bfa6b49bb1bd0132493260a41f70b5e297
DamianIH <u201924756@upc.edu.pe>
2025-05-13 21:36:28 -0500

    feat: added register and info component
    Se agrego componentes para complementar el login

--- src/app/Authentication/Information/information/information.component.ts
+++ src/app/Authentication/Information/information/information.component.ts
@@ -2,27 +2,21 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 
-
 @Component({
-  selector: 'app-login',
+  selector: 'app-information',
   imports: [
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatTabsModule,
     MatButtonModule,
     RouterLink
   ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  templateUrl: './information.component.html',
+  styleUrl: './information.component.css'
 })
-export class LoginComponent {
-  
+export class InformationComponent {
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { CompanyProfileComponent } from './Authentication/CompanyProfile/company-profile/company-profile.component';
 import { ProfileComponent } from './Authentication/Profile/profile/profile.component';
+import { LoginComponent } from './Authentication/Login/login/login.component';
+import { RegisterComponent } from './Authentication/Register/register/register.component';
+import { InformationComponent } from './Authentication/Information/information/information.component';
 
 
 export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch:'full'
+    },
     {
         path: 'company',
         component: CompanyProfileComponent
@@ -11,5 +19,17 @@ export const routes: Routes = [
     {
         path: 'profile',
         component: ProfileComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'info',
+        component: InformationComponent
     }
 ];
